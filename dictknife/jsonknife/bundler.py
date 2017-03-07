a2ab6d9b9434e8921024d4726501b516b47662b8
podhmo <ababjam61+github@gmail.com>
2017-03-08 01:52:40 +0900

    separation

--- dictknife/jsonknife/bundler.py
+++ dictknife/jsonknife/bundler.py
@@ -14,6 +14,100 @@ logger = logging.getLogger("jsonknife.bundler")
 CacheItem = namedlist("CacheItem", "file, localref, globalref, resolver, data")
 
 
+class Bundler(object):
+    def __init__(self, resolver, strict=False):
+        self.resolver = resolver
+        self.accessor = CachedItemAccessor(resolver)
+        self.item_map = {}  # localref -> item
+        self.strict = strict
+
+    @reify
+    def scanner(self):
+        return Scanner(self.accessor, self.item_map, strict=self.strict)
+
+    @reify
+    def emitter(self):
+        return Emitter(self.accessor, self.item_map)
+
+    def bundle(self, doc=None):
+        doc = doc or self.resolver.doc
+        self.scanner.scan(doc)
+        return self.emitter.emit(self.resolver, doc)
+
+
+class Scanner(object):
+    def __init__(self, accessor, item_map, strict=False):
+        self.accessor = accessor
+        self.item_map = item_map
+        self.strict = strict
+
+    @reify
+    def ref_walking(self):
+        return LooseDictWalkingIterator(["$ref"])
+
+    @reify
+    def conflict_fixer(self):  # todo: rename
+        return SimpleConflictFixer(self.item_map, strict=self.strict)
+
+    @reify
+    def localref_fixer(self):  # todo: rename
+        return SwaggerLocalrefFixer()
+
+    def scan(self, doc):
+        for path, sd in self.ref_walking.iterate(doc):
+            try:
+                item = self.accessor.access_and_stacked(sd["$ref"])
+                item = self.localref_fixer.fix_localref(path, item)
+                if item.localref not in self.item_map:
+                    self.item_map[item.localref] = item
+                    self.scan(doc=item.data)
+                if item.globalref != self.item_map[item.localref].globalref:
+                    newitem = self.conflict_fixer.fix_conflict(self.item_map[item.localref], item)
+                    self.scan(doc=newitem.data)
+            finally:
+                self.accessor.pop_stack()
+
+
+class Emitter(object):
+    def __init__(self, accessor, item_map):
+        self.raw_accessor = Accessor()
+        self.accessor = accessor
+        self.item_map = item_map
+
+    @reify
+    def ref_walking(self):
+        return LooseDictWalkingIterator(["$ref"])
+
+    def get_item_by_globalref(self, globalref):
+        return self.accessor.cache[globalref]
+
+    def get_item_by_localref(self, localref):
+        return self.item_map[localref]
+
+    def emit(self, resolver, doc):
+        # side effect
+        d = OrderedDict()
+        for path, sd in self.ref_walking.iterate(doc):
+            self.replace_ref(resolver, sd)
+
+        d = deepmerge(d, doc)
+        for name, item in self.item_map.items():
+            if name == "":
+                continue
+            data = item.data
+            for path, sd in self.ref_walking.iterate(data):
+                self.replace_ref(item.resolver, sd)
+            self.raw_accessor.assign(d, name.split("/"), data)
+        return d
+
+    def replace_ref(self, resolver, sd):
+        filename, _, pointer = resolver.resolve_pathset(sd["$ref"])
+        related = self.get_item_by_globalref((filename, pointer))
+        new_ref = "#/{}".format(related.localref)
+        logger.debug("fix ref: %r -> %r (where=%r)", sd["$ref"], new_ref, resolver.filename)
+        sd["$ref"] = new_ref
+
+
 class SwaggerLocalrefFixer(object):  # todo: rename
     prefixes = set(["definitions", "paths", "responses", "parameters"])
 
@@ -50,8 +144,9 @@ class SwaggerLocalrefFixer(object):  # todo: rename
 
 
 def SimpleConflictFixer(object):  # todo: rename
-    def __init__(self, item_map):
+    def __init__(self, item_map, strict=False):
         self.item_map = item_map
+        self.strict = strict
 
     def fix_conflict(self, olditem, newitem):
         msg = "conficted. {!r} <-> {!r}".format(olditem.globalref, newitem.globalref)
@@ -68,72 +163,6 @@ def SimpleConflictFixer(object):  # todo: rename
             i += 1
         self.item_map[newitem.localref] = newitem
         return newitem
-
-
-class Bundler(object):
-    def __init__(self, resolver, strict=False):
-        self.raw_accessor = Accessor()
-        self.accessor = CachedItemAccessor(resolver)
-        self.resolver = resolver
-        self.item_map = {}  # localref -> item
-        self.strict = strict
-
-    @reify
-    def localref_fixer(self):  # todo: rename
-        return SwaggerLocalrefFixer()
-
-    @reify
-    def conflict_fixer(self):  # todo: rename
-        return SimpleConflictFixer(self.item_map)
-
-    def get_item_by_globalref(self, globalref):
-        return self.accessor.cache[globalref]
-
-    def get_item_by_localref(self, localref):
-        return self.item_map[localref]
-
-    @reify
-    def ref_walking(self):
-        return LooseDictWalkingIterator(["$ref"])
-
-    def scan(self, doc):
-        for path, sd in self.ref_walking.iterate(doc):
-            try:
-                item = self.accessor.access_and_stacked(sd["$ref"])
-                item = self.localref_fixer.fix_localref(path, item)
-                if item.localref not in self.item_map:
-                    self.item_map[item.localref] = item
-                    self.scan(doc=item.data)
-                if item.globalref != self.item_map[item.localref].globalref:
-                    newitem = self.conflict_fixer.fix_conflict(self.item_map[item.localref], item)
-                    self.scan(doc=newitem.data)
-            finally:
-                self.accessor.pop_stack()
-
-    def fix_ref(self, resolver, sd):
-        filename, _, pointer = resolver.resolve_pathset(sd["$ref"])
-        related = self.get_item_by_globalref((filename, pointer))
-        new_ref = "#/{}".format(related.localref)
-        logger.debug("fix ref: %r -> %r (where=%r)", sd["$ref"], new_ref, resolver.filename)
-        sd["$ref"] = new_ref
-
-    def bundle(self, doc=None):
-        doc = doc or self.resolver.doc
-        self.scan(doc)
-        # side effect
-        d = OrderedDict()
-        for path, sd in self.ref_walking.iterate(doc):
-            self.fix_ref(self.resolver, sd)
-
-        d = deepmerge(d, doc)
-        for name, item in self.item_map.items():
-            if name == "":
-                continue
-            data = item.data
-            for path, sd in self.ref_walking.iterate(data):
-                self.fix_ref(item.resolver, sd)
-            self.raw_accessor.assign(d, name.split("/"), data)
-        return d
 
 
 class CachedItemAccessor(StackedAccessor):
