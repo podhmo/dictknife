add75d04bbf2423878d296d62be30a4b8cc79942
podhmo <ababjam61+github@gmail.com>
2017-03-25 19:53:06 +0900

    rename flatten

--- dictknife/jsonknife/__init__.py
+++ dictknife/jsonknife/__init__.py
@@ -1,5 +1,5 @@
 from .expander import Expander  # NOQA
 from .bundler import Bundler  # NOQA
-from .lifting import lifting_jsonschema_definition  # NOQA
+from .lifting import lifting_jsonschema_definition, flatten  # NOQA
 from .resolver import get_resolver_from_filename  # NOQA
 from .samplevalue import SampleValuePlotter  # NOQA
--- dictknife/jsonknife/lifting.py
+++ dictknife/jsonknife/lifting.py
@@ -83,8 +83,9 @@ class SubDefinitionLifting:
         return self.return_definition(data, fullname, typ="array")
 
 
-# TODO: rename to flatten
-def lifting_jsonschema_definition(data, replace=True):
+def flatten(data, replace=True):
+    if "definitions" not in data:
+        return data
     w = SubDefinitionLifting(replace=replace)
     for name in list(data["definitions"].keys()):
         prop = data["definitions"].pop(name)
@@ -92,3 +93,6 @@ def lifting_jsonschema_definition(data, replace=True):
         extracted[name] = prop
         data["definitions"].update(reversed(extracted.items()))
     return data
+
+# BC
+lifting_jsonschema_definition = flatten
