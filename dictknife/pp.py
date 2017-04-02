04b2c2384882b900e5c1b5c6861c4fafcf91fc55
podhmo <ababjam61+github@gmail.com>
2017-04-03 03:13:42 +0900

    indent

--- dictknife/pp.py
+++ dictknife/pp.py
@@ -1,10 +1,28 @@
 import json
 import sys
+import contextlib
+from io import StringIO
 
 
-def pp(d, out=sys.stdout):
+def pp(d, out=None):
+    out = out or sys.stdout
     try:
         json.dump(d, out, sort_keys=True, indent=2, ensure_ascii=False, default=str)
     except TypeError:
         # xxx: such as `unorderable types: NoneType() < str()`
         json.dump(d, out, sort_keys=False, indent=2, ensure_ascii=False, default=str)
+
+
+@contextlib.contextmanager
+def indent(n):
+    buf = StringIO()
+    with contextlib.redirect_stdout(buf):
+        yield buf
+    buf.seek(0)
+
+    prefix = " " * n
+    write = sys.stdout.write
+    for line in buf:
+        write(prefix)
+        write(line)
+    sys.stdout.flush()
