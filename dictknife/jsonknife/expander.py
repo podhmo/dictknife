5c79c4e8713df0edfa365af08d9c5c00ae70b92d
podhmo <ababjam61+github@gmail.com>
2017-04-07 02:49:58 +0900

    mutal recursion

--- dictknife/jsonknife/expander.py
+++ dictknife/jsonknife/expander.py
@@ -1,12 +1,6 @@
 from dictknife import LooseDictWalkingIterator
 from dictknife.langhelpers import reify
 from .accessor import StackedAccessor
-from dictknife.contexts import PathContext
-
-
-class SelfContext(PathContext):
-    def __call__(self, walker, fn, value):
-        return fn(self, value)
 
 
 class Expander(object):
@@ -16,13 +10,27 @@ class Expander(object):
 
     @reify
     def ref_walking(self):
-        return LooseDictWalkingIterator(["$ref"], context_factory=SelfContext)
+        return LooseDictWalkingIterator(["$ref"])
 
     def access(self, ref):
         return self.accessor.access_and_stacked(ref)
 
     def expand(self):
         return self.expand_subpart(self.resolver.doc)
+
+    def _find_self(self, me, d):
+        if id(me) == id(d):
+            return True
+        elif isinstance(d, dict):
+            for v in d.values():
+                if self._find_self(me, v):
+                    return True
+        elif isinstance(d, list):
+            for x in d:
+                if self._find_self(me, x):
+                    return True
+        else:
+            return False
 
     def expand_subpart(self, subpart, resolver=None, ctx=None):
         resolver = resolver or self.resolver
@@ -33,7 +41,7 @@ class Expander(object):
                 ref = subpart.pop("$ref")
                 new_subpart = self.expand_subpart(original, resolver=self.accessor.resolver, ctx=ctx)
                 # for circular reference
-                if id(new_subpart) in ctx._arrived:
+                if self._find_self(subpart, new_subpart):
                     subpart["$ref"] = ref
                 else:
                     subpart.update(new_subpart)
@@ -41,6 +49,6 @@ class Expander(object):
                 self.accessor.pop_stack()
             return subpart
         else:
-            for ctx, sd in self.ref_walking.iterate(subpart):
+            for path, sd in self.ref_walking.iterate(subpart):
                 self.expand_subpart(sd, resolver=resolver, ctx=ctx)
             return subpart
--- examples/parser/main.py
+++ examples/parser/main.py
@@ -27,10 +27,22 @@ definitions:
             null
 """.strip()
 
+defs2 = """
+definitions:
+  text_assets:
+    properties:
+      asset:
+        $ref: "#/definitions/asset_image"
+  asset_image:
+    properties:
+      caption:
+        $ref: "#/definitions/text_assets"
+"""
+
 from dictknife.jsonknife.resolver import OneDocResolver
 from dictknife.jsonknife.expander import Expander
 from dictknife import pp
-doc = loads(defs)
+doc = loads(defs2)
 resolver = OneDocResolver(doc)
 expander = Expander(resolver)
 pp(expander.expand())
