860f27f9de28f376ba943b7692dc09112d7e73ca
podhmo <ababjam61+github@gmail.com>
2017-05-01 14:41:40 +0900

    bump version

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ testing_extras = tests_require + [
 ]
 
 setup(name='dictknife',
-      version='0.4.2',
+      version='0.4.3',
       description='utility set of handling dict',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
