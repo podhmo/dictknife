6cedfa06c631cc75832115a2150209c5e04b4485
podhmo <ababjam61+github@gmail.com>
2017-06-21 22:20:08 +0900

    0.5.0

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ testing_extras = tests_require + [
 
 setup(
     name='dictknife',
-    version='0.4.8',
+    version='0.5.0',
     description='utility set of handling dict',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
