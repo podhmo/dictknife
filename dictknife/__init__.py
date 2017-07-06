40e806b32fc6f244d3e1ee6604e0145e26eb00a3
podhmo <ababjam61+github@gmail.com>
2017-07-07 05:59:29 +0900

    drop LooseDictWalker and dictknife.chain #15

--- dictknife/__init__.py
+++ dictknife/__init__.py
@@ -1,6 +1,4 @@
-from dictknife.walkers import LooseDictWalker  # NOQA
-from dictknife.walkers import LooseDictWalkingIterator  # NOQA
-from dictknife.chain import ChainSource, chain  # NOQA
+from dictknife.walkers import LooseDictWalkingIterator, DictWalker  # NOQA
 from dictknife.accessor import Accessor  # NOQA
 from dictknife.deepmerge import deepmerge  # NOQA
 from dictknife.deepequal import deepequal  # NOQA
--- dictknife/walkers.py
+++ dictknife/walkers.py
@@ -2,61 +2,6 @@ from collections import deque
 from .operators import apply
 from .contexts import PathContext
 
-# - LooseDictWalker is like a internal iterator
-# - LooseDictWalkingIterator is like a external iterator
-# xxx: in the future, which one is deleted.
-
-
-class LooseDictWalker(object):
-    context_factory = PathContext
-
-    def __init__(self, on_container=None, on_data=None, context_factory=None):
-        self.on_container = on_container
-        self.on_data = on_data
-        self.context_factory = context_factory or self.__class__.context_factory
-
-    def on_found(self, ctx, d, k):
-        if self.on_container is not None:
-            ctx(self, self.on_container, d)
-        if self.on_data is not None:
-            ctx(self, self.on_data, d[k])
-
-    def create_context(self, ctx=None):
-        return ctx or self.context_factory()
-
-    def walk(self, qs, d, depth=-1, ctx=None):
-        ctx = self.create_context(ctx)
-        return self._walk(ctx, deque(qs), d, depth=depth)
-
-    def _walk(self, ctx, qs, d, depth):
-        if depth == 0:
-            return
-
-        if not qs:
-            return
-
-        if hasattr(d, "keys"):
-            for k in list(d.keys()):
-                ctx.push(k)
-                if apply(qs[0], k):
-                    q = qs.popleft()
-                    self._walk(ctx, qs, d[k], depth - 1)
-                    if len(qs) == 0:
-                        self.on_found(ctx, d, k)
-                    qs.appendleft(q)
-                else:
-                    self._walk(ctx, qs, d[k], depth)
-                ctx.pop()
-            return
-        elif isinstance(d, (list, tuple)):
-            ctx.push("[]")
-            for e in d:
-                self._walk(ctx, qs, e, depth)
-            ctx.pop()
-            return
-        else:
-            return
-
 
 class ContainerHandler(object):
     def identity(self, *args):
@@ -74,7 +19,7 @@ class DataHandler(object):
         return ctx(walker, self.identity, d[k])
 
 
-class LooseDictWalkingIterator(object):
+class DictWalker(object):
     context_factory = PathContext
     handler_factory = ContainerHandler
 
@@ -122,3 +67,6 @@ class LooseDictWalkingIterator(object):
             return
         else:
             return
+
+
+LooseDictWalkingIterator = DictWalker  # NOQA for backward comaptibility
