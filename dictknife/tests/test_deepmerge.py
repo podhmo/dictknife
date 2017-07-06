9139482d0096732f0994dda351ae000f1c147144
podhmo <ababjam61+github@gmail.com>
2017-07-07 05:54:27 +0900

    add test #14

--- dictknife/tests/test_deepmerge.py
+++ dictknife/tests/test_deepmerge.py
@@ -15,26 +15,9 @@ class TestDeepMerge(unittest.TestCase):
                 "y": 10
             },
         }
-        d1 = {
-            "a": {
-                "x": 1
-            },
-            "b": {
-                "z": 10
-            },
-            "c": 100
-        }
+        d1 = {"a": {"x": 1}, "b": {"z": 10}, "c": 100}
         actual = self._callFUT(d0, d1)
-        expected = {
-            "a": {
-                "x": 1
-            },
-            "b": {
-                "y": 10,
-                "z": 10
-            },
-            "c": 100
-        }
+        expected = {"a": {"x": 1}, "b": {"y": 10, "z": 10}, "c": 100}
         self.assertEqual(actual, expected)
         self.assertNotEqual(actual, d0, msg="not modified!!")
 
@@ -54,7 +37,11 @@ class TestDeepMerge(unittest.TestCase):
                 "z": 3,
             },
             "children": [1, 2, 3],
-            "a": {"b": {"c": "d"}},
+            "a": {
+                "b": {
+                    "c": "d"
+                }
+            },
         }
         actual = self._callFUT(d0, d1, override=True)
         expected = {
@@ -65,7 +52,26 @@ class TestDeepMerge(unittest.TestCase):
                 "z": 3,
             },
             "children": [1, 2, 3],
-            "a": {"b": {"c": "d"}},
+            "a": {
+                "b": {
+                    "c": "d"
+                }
+            },
         }
         self.assertEqual(actual, expected)
         self.assertNotEqual(actual, d0, msg="not modified!!")
+
+    def test_with_empty(self):
+        from collections import namedtuple
+        C = namedtuple("C", "d0 d1 override expected")
+
+        candidates = [
+            C(d0={1: 10}, d1=None, override=False, expected={1: 10}),
+            C(d0={1: 10}, d1={}, override=False, expected={1: 10}),
+            C(d0={1: 10}, d1=None, override=True, expected={1: 10}),
+            C(d0={1: 10}, d1={}, override=True, expected={1: 10}),
+        ]
+        for c in candidates:
+            with self.subTest(d0=c.d0, d1=c.d1):
+                actual = self._callFUT(c.d0, c.d1, override=c.override)
+                self.assertEqual(actual, c.expected)
