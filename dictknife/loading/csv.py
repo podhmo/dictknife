9e864f38e8fa55a75e503dbd5628cbe0f241ce37
podhmo <ababjam61+github@gmail.com>
2017-11-22 21:46:45 +0900

    ordered output

--- dictknife/loading/csv.py
+++ dictknife/loading/csv.py
@@ -1,8 +1,32 @@
 from ._lazyimport import m
+from collections import OrderedDict
 
 
 def load(fp, *, loader=None, delimiter=",", **kwargs):
-    reader = m.csv.DictReader(fp, delimiter=delimiter)
+    class OrderedDictReader(m.csv.DictReader):
+        def __next__(self):
+            if self.line_num == 0:
+                # Used only for its side effect.
+                self.fieldnames
+            row = next(self.reader)
+            self.line_num = self.reader.line_num
+
+            # unlike the basic reader, we prefer not to return blanks,
+            # because we will typically wind up with a dict full of None
+            # values
+            while row == []:
+                row = next(self.reader)
+            d = OrderedDict(zip(self.fieldnames, row))
+            lf = len(self.fieldnames)
+            lr = len(row)
+            if lf < lr:
+                d[self.restkey] = row[lf:]
+            elif lf > lr:
+                for key in self.fieldnames[lr:]:
+                    d[key] = self.restval
+            return d
+
+    reader = OrderedDictReader(fp, delimiter=delimiter)
     return reader
 
 
@@ -15,7 +39,9 @@ def dump(rows, fp, *, delimiter=","):
     itr = iter(rows)
     first_row = next(itr)
     fields = list(first_row.keys())
-    writer = m.csv.DictWriter(fp, fields, delimiter=delimiter, lineterminator="\r\n")
+    writer = m.csv.DictWriter(
+        fp, fields, delimiter=delimiter, lineterminator="\r\n", quoting=m.csv.QUOTE_ALL
+    )
     writer.writeheader()
     writer.writerow(first_row)
     writer.writerows(itr)
