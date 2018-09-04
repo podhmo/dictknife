07d1ad74330845f7b486945019a8c822608cb81f
podhmo <ababjam61+github@gmail.com>
2018-09-04 12:01:38 +0900

    feat: loader cache

--- dictknife/loading/spreadsheet.py
+++ dictknife/loading/spreadsheet.py
@@ -1,9 +1,15 @@
 import contextlib
 from ._lazyimport import m
 
+_loader = None
 
-def load(pattern, *, errors=None, **kwargs):
-    return m.gsuite.Loader().load_sheet(pattern)
+
+def load(pattern, *, errors=None, loader=None, **kwargs):
+    global _loader
+    loader = loader or _loader
+    if _loader is None:
+        _loader = m.gsuite.Loader()
+    return _loader.load_sheet(pattern)
 
 
 def dump(rows, fp, *, sort_keys=False):
