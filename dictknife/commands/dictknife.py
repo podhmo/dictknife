38ce7ff39452109f05a6ce85132fabbe2fac9f93
podhmo <ababjam61+github@gmail.com>
2018-09-05 01:13:50 +0900

    tests: red tests

--- dictknife/commands/dictknife.py
+++ dictknife/commands/dictknife.py
@@ -50,7 +50,6 @@ def cat(
                     sd = (loading.loads(line, format=input_format) for line in rf)
                 else:
                     sd = loading.load(rf, format=input_format, errors=errors)
-
                 if size is not None:
                     sd = itertools.islice(sd, size)
 
