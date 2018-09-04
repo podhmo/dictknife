06bb8576792190c83a5b7f7dfbe7450d708befbc
podhmo <ababjam61+github@gmail.com>
2018-09-05 01:55:39 +0900

    optional: discovery cache

--- dictknife/loading/_gsuite.py
+++ dictknife/loading/_gsuite.py
@@ -2,20 +2,23 @@ import typing as t
 import sys
 import os.path
 import logging
+import pickle
 import httplib2
 from oauth2client import file, client
 from oauth2client import tools
 from oauth2client.clientsecrets import InvalidClientSecretsError
 from oauth2client.client import OAuth2Credentials
 from dictknife.langhelpers import reify
-from googleapiclient.discovery_cache import LOGGER as noisy_logger
-# supress stderr message of 'ImportError: file_cache is unavailable when using oauth2client >= 4.0.0 or google-auth' # noqa
-noisy_logger.setLevel(logging.ERROR)  # noqa
+from googleapiclient.discovery_cache.base import Cache
+# from googleapiclient.discovery_cache import LOGGER as noisy_logger
+# # supress stderr message of 'ImportError: file_cache is unavailable when using oauth2client >= 4.0.0 or google-auth' # noqa
+# noisy_logger.setLevel(logging.ERROR)  # noqa
 import googleapiclient.discovery
 
 logger = logging.getLogger(__name__)
 
 DEFAULT_CREDENTIALS_PATH = "~/.config/dictknife/credentials.json"
+DEFAULT_DISCOVERY_CACHE_PATH = "~/.config/dictknife/discovery.pickle"
 
 # Authorize using one of the following scopes:
 #     'https://www.googleapis.com/auth/drive'
@@ -81,26 +84,63 @@ def get_credentials_failback_webbrowser(
             input("saved? (if saved, please typing enter key)")
 
 
+class MemoryCache(Cache):
+    def __init__(self):
+        self.cache = {}
+
+    def get(self, url):
+        return self.cache.get(url)
+
+    def set(self, url, content):
+        self.cache[url] = content
+
+    @property
+    def is_empty(self):
+        return len(self.cache) == 0
+
+
+def _get_discovery_cache(path):
+    if not os.path.exists(path):
+        return MemoryCache()
+
+    with open(path, "rb") as rf:
+        return pickle.load(rf)
+
+
 class Loader:
     def __init__(
         self,
         *,
         config_path=DEFAULT_CREDENTIALS_PATH,
+        discovery_cache_path=DEFAULT_DISCOVERY_CACHE_PATH,
         scopes=[SCOPE],
         get_credentials=get_credentials_failback_webbrowser,
         http=None
     ):
-        self.config_path = config_path
+        self.config_path = os.path.expanduser(config_path)
+        self.discovery_cache_path = os.path.expanduser(discovery_cache_path)
         self.scopes = scopes
         self.get_credentials = get_credentials
         self.http = http or httplib2.Http()
 
     @reify
+    def cache(self):
+        return _get_discovery_cache(self.discovery_cache_path)
+
+    def _save_cache(self, cache):
+        with open(self.discovery_cache_path, "wb") as wf:
+            pickle.dump(cache, wf)
+
+    @reify
     def service(self):
+        need_save = self.cache.is_empty
         credentials = self.get_credentials(self.config_path, scopes=self.scopes)
-        return googleapiclient.discovery.build(
-            'sheets', 'v4', http=credentials.authorize(self.http)
+        service = googleapiclient.discovery.build(
+            'sheets', 'v4', http=credentials.authorize(self.http), cache=self.cache
         )
+        if need_save:
+            self._save_cache(self.cache)
+        return service
 
     def load_sheet(self, guessed, *, with_header=True):
         resource = self.service.spreadsheets()
