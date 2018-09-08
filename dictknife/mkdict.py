1aa272aad2471be50d91e14de65d92142a8d9aec
podhmo <ababjam61+github@gmail.com>
2018-09-09 02:03:26 +0900

    feat: mkdict function

--- dictknife/mkdict.py
+++ dictknife/mkdict.py
@@ -1,11 +1,27 @@
 import shlex
+from .guessing import guess
+from .accessing import Accessor
+from .langhelpers import make_dict
 
 
-def mkdict(xs):
-    pass
+def mkdict(line, *, separator="/", accessor=Accessor(make_dict), guess=guess):
+    d = make_dict()
+    tokens = iter(tokenize(line))
+    return _mkdict(d, tokens, separator=separator, accessor=accessor, guess=guess)
 
 
-def parse(line):
+def _mkdict(d, tokens, *, separator, accessor, guess):
+    while True:
+        try:
+            k = next(tokens)
+            v = next(tokens)
+            accessor.assign(d, k.split(separator), guess(v))
+        except StopIteration:
+            break
+    return d
+
+
+def tokenize(line):
     lexer = shlex.shlex(line, punctuation_chars=True)
     lexer.whitespace += "="
     for token in lexer:
--- dictknife/tests/test_mkdict.py
+++ dictknife/tests/test_mkdict.py
@@ -1,10 +1,10 @@
 import unittest
 
 
-class ParseTests(unittest.TestCase):
+class TokenizeTests(unittest.TestCase):
     def _callFUT(self, *args, **kwargs):
-        from dictknife.mkdict import parse
-        return parse(*args, **kwargs)
+        from dictknife.mkdict import tokenize
+        return tokenize(*args, **kwargs)
 
     def test_it(self):
         from collections import namedtuple
@@ -17,8 +17,41 @@ class ParseTests(unittest.TestCase):
         ]
         for c in candidates:
             with self.subTest(input=c.input):
-                got = list(self._callFUT(c.input))
-                self.assertEqual(got, c.output)
+                got = self._callFUT(c.input)
+                self.assertEqual(list(got), c.output)
+
+
+class MkDictTests(unittest.TestCase):
+    def _getTarget(self):
+        from dictknife.mkdict import _mkdict
+        return _mkdict
+
+    def _callFUT(self, tokens):
+        from dictknife.accessing import Accessor
+        from dictknife.guessing import guess
+        return self._getTarget()({}, iter(tokens), separator="/", accessor=Accessor(), guess=guess)
+
+    def test_it(self):
+        from collections import namedtuple
+        C = namedtuple("C", "tokens, expected")
+        # yapf: disable
+        candidates = [
+            C(tokens=["name", "foo"],
+              expected={"name": "foo"}),
+            C(tokens=["name", "foo", "age", "20"],
+              expected={"name": "foo", "age": 20}),
+            C(tokens=["name", "foo", "age", 20],
+              expected={"name": "foo", "age": 20}),
+            C(tokens=["name", "foo", "age", 20, "parent/name", "bar"],
+              expected={"name": "foo", "age": 20, "parent": {"name": "bar"}}),
+            C(tokens=["name", "foo", "age", 20, "parent/name", "bar", "parent/name", "*overwrite*"],
+              expected={"name": "foo", "age": 20, "parent": {"name": "*overwrite*"}}),
+        ]
+        # yapf: enable
+        for c in candidates:
+            with self.subTest(tokens=c.tokens):
+                got = self._callFUT(c.tokens)
+                self.assertDictEqual(dict(got), c.expected)
 
 
 if __name__ == "__main__":
