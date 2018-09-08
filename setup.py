ec5662fea2245b8a7b6a2914110ef58acfe6fc0a
podhmo <ababjam61+github@gmail.com>
2018-09-09 00:44:37 +0900

    adjustment setup.py

--- setup.py
+++ setup.py
@@ -11,36 +11,6 @@ try:
 except IOError:
     README = CHANGES = ''
 
-install_requires = []
-
-docs_extras = [
-    "sphinx",
-    "recommonmark",
-    "sphinx_rtd_theme",
-]
-
-load_extras = [
-    "PyYAML",
-    "toml",
-]
-
-spreadsheet_extrs = [
-    "google-api-python-client",
-    "oauth2client",
-]
-
-command_extras = [
-    "PyYAML",
-    "magicalimport",
-    "prestring",
-]
-
-tests_require = []
-
-testing_extras = tests_require + [
-    "PyYAML",
-]
-
 setup(
     name='dictknife',
     version='0.8.2',
@@ -57,15 +27,31 @@ setup(
     packages=find_packages(exclude=["dictknife.tests"]),
     include_package_data=True,
     zip_safe=False,
-    install_requires=install_requires,
+    install_requires=[],
     extras_require={
-        'testing': testing_extras,
-        'docs': docs_extras,
-        'load': load_extras,
-        'command': command_extras,
-        'spreadsheet': spreadsheet_extrs,
+        'testing': [
+            "PyYAML",
+        ],
+        'docs': [
+            "sphinx",
+            "recommonmark",
+            "sphinx_rtd_theme",
+        ],
+        'load': [
+            "PyYAML",
+            "toml",
+        ],
+        'command': [
+            "PyYAML",
+            "magicalimport",
+            "prestring",
+        ],
+        'spreadsheet': [
+            "google-api-python-client",
+            "oauth2client",
+        ],
     },
-    tests_require=tests_require,
+    tests_require=[],
     test_suite="dictknife.tests",
     entry_points="""
       [console_scripts]
