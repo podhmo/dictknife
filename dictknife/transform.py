6715c2a78bad56e75272f3e55eb8dd2a480140c5
podhmo <ababjam61+github.com@gmail.com>
2018-10-16 16:45:15 +0900

    flatten

---
+++ dictknife/transform.py
@@ -0,0 +1,21 @@
+def _make_key(k0, k1, *, sep="/"):
+    if k1 is None:
+        return k0
+    return "{}{}{}".format(k0, sep, k1)
+
+
+def flatten(d, *, sep="/"):
+    if isinstance(d, (list, tuple)):
+        return {
+            _make_key(k, i, sep=sep): v
+            for i, row in enumerate(d) for k, v in flatten(row).items()
+        }
+    elif hasattr(d, "get"):
+        return {
+            _make_key(k, k2, sep=sep): v2
+            for k, v in d.items() for k2, v2 in flatten(v, sep=sep).items()
+        }
+    elif hasattr(d, "__next__"):
+        return flatten(list(d), sep=sep)
+    else:
+        return {None: d}
