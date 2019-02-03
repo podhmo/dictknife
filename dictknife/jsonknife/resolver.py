c4463b992362496dd344d740f5ed40109bcfaad5
podhmo <ababjam61+github@gmail.com>
2019-02-03 17:26:50 +0900

    adding accessor method

--- dictknife/jsonknife/resolver.py
+++ dictknife/jsonknife/resolver.py
@@ -1,13 +1,25 @@
 import sys
 import logging
 import os.path
-from dictknife import loading
-from dictknife.langhelpers import reify, pairrsplit
+from .. import loading
+from ..langhelpers import reify, pairrsplit
 from . import relpath
+from .accessor import (
+    access_by_json_pointer,
+    assign_by_json_pointer,
+)
 logger = logging.getLogger("jsonknife.resolver")
 
 
-class OneDocResolver(object):
+class AccessorMixin:
+    def access(self, doc, jsonref):
+        return access_by_json_pointer(doc, jsonref)
+
+    def assign(self, doc, jsonref, value):
+        return assign_by_json_pointer(doc, jsonref, value)
+
+
+class OneDocResolver(AccessorMixin):
     def __init__(self, doc, name="*root*", onload=None):
         self.doc = doc
         self.name = name
@@ -22,7 +34,7 @@ class OneDocResolver(object):
         return self, query[1:]
 
 
-class ExternalFileResolver(object):
+class ExternalFileResolver(AccessorMixin):
     def __init__(
         self,
         filename,
@@ -115,7 +127,7 @@ class ROOT:
     history = []
 
 
-def get_resolver_from_filename(filename, loader=loading, doc=None, onload=None):
+def get_resolver(filename, loader=loading, doc=None, onload=None):
     if filename is None:
         doc = doc or loading.load(sys.stdin)
         return OneDocResolver(doc, onload=onload)
@@ -124,3 +136,7 @@ def get_resolver_from_filename(filename, loader=loading, doc=None, onload=None):
         if doc:
             resolver.doc = doc
         return resolver
+
+
+# for backward compatibility
+get_resolver_from_filename = get_resolver
