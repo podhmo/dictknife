20a95b35666634d75e318c5da3ab3faf9f7a20e8
podhmo <ababjam61+github@gmail.com>
2019-02-03 17:12:05 +0900

    add shortcut, bundle()

--- dictknife/jsonknife/__init__.py
+++ dictknife/jsonknife/__init__.py
@@ -3,3 +3,9 @@ from .bundler import Bundler  # NOQA
 from .resolver import get_resolver_from_filename  # NOQA
 from .example import extract as extract_example  # NOQA
 from .accessor import access_by_json_pointer, assign_by_json_pointer  # NOQA
+
+
+def bundle(filename, *, onload=None, doc=None):
+    resolver = get_resolver_from_filename(filename, doc=doc, onload=onload)
+    bundler = Bundler(resolver)
+    return bundler.bundle()
