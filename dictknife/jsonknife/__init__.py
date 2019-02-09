84e43e17eba613740a25f28e4d81069a38765b41
podhmo <ababjam61+github@gmail.com>
2019-02-10 01:17:22 +0900

    fix: jsonknife bundle, not using build_subset()

--- dictknife/jsonknife/__init__.py
+++ dictknife/jsonknife/__init__.py
@@ -3,7 +3,6 @@ from .bundler import Bundler  # noqa
 from .resolver import (  # noqa
     get_resolver,
     get_resolver_from_filename,  # backward compatibility
-    build_subset,
 )
 from .example import extract as extract_example  # noqa
 from .accessor import (  # noqa
@@ -11,14 +10,23 @@ from .accessor import (  # noqa
     assign_by_json_pointer,
     path_to_json_pointer,
 )
+from ..langhelpers import make_dict
+import os.path
 
 
 def bundle(filename, *, jsonref=None, onload=None, doc=None, format=None):
     if "#/" in filename:
         filename, jsonref = filename.rsplit("#/", 1)
-    resolver = get_resolver_from_filename(filename, doc=doc, onload=onload, format=None)
-    bundler = Bundler(resolver)
-    doc = resolver.doc
+
     if jsonref is not None:
-        doc = build_subset(resolver, jsonref)
-    return bundler.bundle(doc)
+        doc = make_dict()
+        assign_by_json_pointer(
+            doc,
+            jsonref,
+            {"$ref": f"{os.path.relpath(filename, start=os.getcwd())}#/{jsonref}"},
+        )
+        filename = os.path.join(os.getcwd(), f"*root*{os.path.splitext(filename)[1]}#/")
+    resolver = get_resolver(filename, doc=doc, onload=onload, format=None)
+    bundler = Bundler(resolver)
+    r = bundler.bundle(resolver.doc)
+    return r
