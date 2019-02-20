f01bafe21c5c083f02596a470ef2d9de59ca1cf1
podhmo <ababjam61+github@gmail.com>
2019-02-20 18:09:41 +0900

    refactor: move normalize_dict to dictknife.transform

--- dictknife/diff/diff.py
+++ dictknife/diff/diff.py
@@ -1,6 +1,6 @@
 import difflib
 from dictknife.deepequal import sort_flexibly
-from ._utils import _normalize_dict
+from dictknife.transform import normalize_dict
 
 
 def diff(
@@ -18,8 +18,8 @@ def diff(
     if normalize:
         d0 = sort_flexibly(d0)
         d1 = sort_flexibly(d1)
-        _normalize_dict(d0)
-        _normalize_dict(d1)
+        normalize_dict(d0)
+        normalize_dict(d1)
 
     # iterator?
     if hasattr(d0, "__next__"):
@@ -30,16 +30,22 @@ def diff(
 
     s0 = tostring(d0, sort_keys=sort_keys).split(terminator)
     s1 = tostring(d1, sort_keys=sort_keys).split(terminator)
-    return difflib.unified_diff(s0, s1, fromfile=fromfile, tofile=tofile, lineterm="", n=n)
+    return difflib.unified_diff(
+        s0, s1, fromfile=fromfile, tofile=tofile, lineterm="", n=n
+    )
 
 
 def _default_tostring(d, *, default=str, sort_keys=True):
     import json
-    return json.dumps(d, indent=2, ensure_ascii=False, sort_keys=sort_keys, default=default)
+
+    return json.dumps(
+        d, indent=2, ensure_ascii=False, sort_keys=sort_keys, default=default
+    )
 
 
 if __name__ == "__main__":
     import datetime
+
     d0 = {"x": datetime.date(2000, 1, 1), "y": {"a": 1, "b": 10}}
     d1 = {"x": datetime.date(2000, 2, 1), "y": {"a": 1, "c": 10}}
     for line in diff(d0, d1):
