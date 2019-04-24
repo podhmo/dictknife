6c658290c77ae5e644c5b69a13267abe283f3f9a
podhmo <ababjam61+github@gmail.com>
2019-04-24 23:47:21 +0900

    feat: stream as iterator

--- dictknife/swaggerknife/stream/__init__.py
+++ dictknife/swaggerknife/stream/__init__.py
@@ -1,20 +1,37 @@
+import typing as t
 from dictknife.jsonknife import get_resolver
+from .event import Event
 from .context import Context
 from .walker import OpenAPIWalker
 
 
 def run(src: str):
-    # todo: to generator
-    # todo: cache
-    # todo: separeted files
-    events = []
-
-    def emit(ev):
+    for ev in make_stream(src):
         print(ev)
-        events.append(ev)
 
-    resolver = get_resolver(src)
-    walker = OpenAPIWalker()
-    ctx = Context(resolver, emit)
 
-    walker.walk(ctx, resolver.doc)
+def make_stream(src: str) -> t.Iterable[Event]:
+    import threading
+    import queue
+
+    q = queue.Queue()
+
+    def provide():
+        resolver = get_resolver(src)
+        walker = OpenAPIWalker()
+        ctx = Context(resolver, emit=q.put)
+
+        try:
+            walker.walk(ctx, resolver.doc)
+        finally:
+            q.put(None)
+
+    th = threading.Thread(target=provide, daemon=True)
+    th.start()
+    while True:
+        v = q.get()
+        if v is None:
+            break
+        yield v
+        q.task_done()
+    th.join()
