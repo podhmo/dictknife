e13c0fe09ccd55f7a5f63bc2afc81ac92db8bbcc
podhmo <ababjam61+github@gmail.com>
2019-07-01 23:04:43 +0900

    refactor: Describer class

--- dictknife/describe.py
+++ dictknife/describe.py
@@ -5,6 +5,41 @@ from .langhelpers import make_dict
 MAX_DEPTH = int(os.environ.get("DICTKNIFE_MAX_DEPTH") or "-1")
 
 
+class Describer:
+    def __init__(self, *, on_last, max_depth=MAX_DEPTH):
+        self.on_last = on_last
+        self.max_depth = max_depth
+
+    def _describe(self, d, *, life: int):
+        if life == 0:
+            return self.on_last(d)
+        else:
+            if isinstance(d, (list, tuple)):
+                return [self._describe(x, life=life - 1) for x in d]
+            elif hasattr(d, "keys"):
+                r = make_dict()
+                for k in d.keys():
+                    r[k] = self._describe(d[k], life=life - 1)
+                return r
+            else:
+                return d  # xxx
+
+    def describe(self, d, *, max_depth=None):
+        life = max_depth
+        if life is None:
+            life = self.max_depth
+        return self._describe(d, life=life)
+
+
+def on_last_default(d):
+    if isinstance(d, (list, tuple)):
+        return {"@type": "list", "@length": len(d), "@max_depth": _depth(d)}
+    elif hasattr(d, "keys"):
+        return {"@type": "dict", "@keys": sorted(d.keys()), "@max_depth": _depth(d)}
+    else:
+        return d  # xxx
+
+
 def _depth(d) -> int:
     if isinstance(d, (list, tuple)):
         return max(_depth(x) for x in d) + 1 if d else 0
@@ -14,35 +49,5 @@ def _depth(d) -> int:
         return 0
 
 
-def _mini_dict(d) -> dict:
-    return {"@type": "dict", "@keys": sorted(d.keys()), "@max_depth": _depth(d)}
-
-
-def _mini_list(d) -> dict:
-    return {"@type": "list", "@length": len(d), "@max_depth": _depth(d)}
-
-
-def describe(d, *, max_depth=None):
-    def _describe(d, *, life):
-        if life == 0:
-            if isinstance(d, (list, tuple)):
-                return _mini_list(d)
-            elif hasattr(d, "keys"):
-                return _mini_dict(d)
-            else:
-                return d  # xxx
-        else:
-            if isinstance(d, (list, tuple)):
-                return [_describe(x, life=life - 1) for x in d]
-            elif hasattr(d, "keys"):
-                r = make_dict()
-                for k in d.keys():
-                    r[k] = _describe(d[k], life=life - 1)
-                return r
-            else:
-                return d  # xxx
-
-    life = max_depth
-    if life is None:
-        life = MAX_DEPTH
-    return _describe(d, life=life)
+def describe(d, *, max_depth=None, describer=Describer(on_last=on_last_default)):
+    return describer.describe(d)
