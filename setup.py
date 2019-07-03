1668a2f522e54402ae2950c9cffecdc9c86d397b
podhmo <ababjam61+github@gmail.com>
2019-07-03 21:47:12 +0900

    chore: use qtoml

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         "testing": ["PyYAML", "jsonpatch"],
         "lint": ["flake8"],
         "docs": ["sphinx", "recommonmark", "sphinx_rtd_theme"],
-        "load": ["PyYAML", "toml"],
+        "load": ["PyYAML", "qtoml"],
         "command": ["PyYAML", "magicalimport", "prestring"],
         "spreadsheet": ["google-api-python-client", "oauth2client"],
     },
