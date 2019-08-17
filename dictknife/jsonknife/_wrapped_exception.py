4919d86914e7364d6599b7a0bbd623e2485a7a71
podhmo <ababjam61+github@gmail.com>
2019-08-17 21:38:45 +0900

    feat: wrapped exception factory

---
+++ dictknife/jsonknife/_wrapped_exception.py
@@ -0,0 +1,36 @@
+from dictknife.langhelpers import reify
+
+
+class WrappedExceptionFactory:
+    def __init__(self, *, prefix="Wrapped", mixin_class=None):
+        self.prefix = prefix
+        self.mixin_class = mixin_class or WrappedMixin
+        self.classes = {}
+
+    def __call__(self, e: Exception, *, where: str) -> Exception:
+        if isinstance(e, self.mixin_class):
+            e.stack.append(where)
+            return e
+
+        cls = self.classes.get(e.__class__)
+        if cls is None:
+            cls = type(
+                f"Wrapped{e.__class__.__name__}", (WrappedMixin, e.__class__), {}
+            )
+            self.classes[e.__class__] = cls
+
+        # sync
+        exc = cls.__new__(cls).with_traceback(e.__traceback__)
+        exc.__dict__.update(e.__dict__)
+        if hasattr(e, "args"):
+            exc.args = e.args
+        return self(exc, where=where)
+
+
+class WrappedMixin:
+    @reify
+    def stack(self):
+        return []
+
+    def __str__(self):
+        return f"{super().__str__()} (where={self.stack})"
