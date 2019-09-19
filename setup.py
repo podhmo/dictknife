b3d26f2d209e55bd01ed17185c58a23b7394658d
podhmo <ababjam61+github@gmail.com>
2019-09-20 00:59:50 +0900

    bump-version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except IOError:
 
 setup(
     name="dictknife",
-    version="0.11.0",
+    version="0.11.1",
     description="utility set of handling dict",
     long_description=README + "\n\n" + CHANGES,
     classifiers=[
