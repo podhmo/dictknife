b4f6e527e037bcdd064cc3a9261b65578c919983
podhmo <ababjam61+github@gmail.com>
2020-04-01 19:20:07 +0900

    Unflatten (#183)
    * feat: transform.unflatten
    
    * chore: update transform examples
    
    * chore: unflatten example
    
    * fix: fix for reversible
    
    * fix: update examples
    
    * chore: update examples

--- dictknife/transform.py
+++ dictknife/transform.py
@@ -1,5 +1,7 @@
 from functools import partial
 from dictknife.langhelpers import as_jsonpointer as _as_jsonpointer
+from dictknife.langhelpers import as_path_node as _as_path_node
+from dictknife import accessing
 from dictknife import naming
 
 
@@ -9,12 +11,36 @@ def _make_key(k0, k1, *, sep="/"):
     return "{}{}{}".format(_as_jsonpointer(str(k0)), sep, k1)
 
 
+def unflatten(d, *, sep="/", accessor=accessing.Accessor()):
+    r = accessor.make_dict()
+    for k, v in d.items():
+        accessor.assign(r, [_as_path_node(x) for x in k.split(sep)], v)
+    return _fix_unflatten_list(r)
+
+
+def _fix_unflatten_list(d):
+    if hasattr(d, "keys"):
+        for k in list(d.keys()):
+            d[k] = _fix_unflatten_list(d[k])
+
+        # list ?
+        if "0" in d and str(len(d) - 1) in d:
+            r = []
+            for i in range(len(d)):
+                k = str(i)
+                if k not in d:
+                    return d
+                r.append(d[k])
+            return r
+    return d
+
+
 def flatten(d, *, sep="/"):
     if isinstance(d, (list, tuple)):
         return {
             _make_key(i, k, sep=sep): v
             for i, row in enumerate(d)
-            for k, v in flatten(row).items()
+            for k, v in flatten(row, sep=sep).items()
         }
     elif hasattr(d, "get"):
         return {
@@ -23,9 +49,11 @@ def flatten(d, *, sep="/"):
             for k2, v2 in flatten(v, sep=sep).items()
         }
     elif hasattr(d, "__next__"):
+        # todo: as generator
         return flatten(list(d), sep=sep)
     else:
-        return {None: _as_jsonpointer(str(d))}
+        # todo: peformance improvement
+        return {None: _as_jsonpointer(d) if hasattr(d, "replace") else d}
 
 
 def rows(d, *, kname="name", vname="value"):
