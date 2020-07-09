3f525494587491ab408cb506bd59f10f5f3a50e4
podhmo <ababjam61+github@gmail.com>
2020-07-10 04:33:00 +0900

    chore: fix for ci

--- dictknife/cliutils/__init__.py
+++ dictknife/cliutils/__init__.py
@@ -11,8 +11,7 @@ def traceback_shortly(debug):
             raise
         else:
             print(
-                "\x1b[33m\x1b[1m{e.__class__.__name__}: {e}\x1b[0m".format(
-                    e=e, file=sys.stderr
-                )
+                "\x1b[33m\x1b[1m{e.__class__.__name__}: {e}\x1b[0m".format(e=e),
+                file=sys.stderr,
             )
             sys.exit(1)
